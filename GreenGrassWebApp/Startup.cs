2d063d467ce241902d87763c7de19e06936c65eb
RENT <RENT@RENT-KOMPUTER>
2018-01-14 12:05:18 +0100

    init comint

---
+++ GreenGrassWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreenGrassWebApp.Startup))]
+namespace GreenGrassWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
